1154c9192bab378fd96fe19d73f4a315ed97ad50
Silvana Siqueira <83840938+Sisi-siqueira@users.noreply.github.com>
2022-10-29 11:32:56 -0300

    codigo

---
+++ interacao.js
@@ -0,0 +1,59 @@
+//crt e control
+const tipoBt = document.getElementById('versao');
+const nomeh1 = document.getElementById('nome');
+const Ps = document.getElementById('Ps');
+const avatar = document.getElementById('avatar');
+const poderP = document.getElementById('poder');
+const fraquezaP = document.getElementById('fraqueza');
+//menu
+const Bt1 = document.getElementById('b1');
+const Bt2 = document.getElementById('b2');
+const Bt3 = document.getElementById('b3');
+const Bt4 = document.getElementById('b4');
+const Bt5 = document.getElementById('b5');
+//logica
+
+Bt1.addEventListener('click',() => {
+    avatar.src = "img/jose.png";
+    nomeh1.innerText='jose';
+    poderP.innerText='fazer merda';
+    fraquezaP.innerText='alcool';
+    Ps.innerText='Ps 12';
+    tipoBt.innerText='basico';
+})
+Bt2.addEventListener('click',() => {
+    avatar.src = "img/ines brasil.png";
+    nomeh1.innerText='ines brasil';
+    poderP.innerText='lacrar';
+    fraquezaP.innerText='falar de mais';
+    Ps.innerText='Ps 20';
+    tipoBt.innerText='não basico';
+})
+Bt3.addEventListener('click',() => {
+    avatar.src = "img/pericles.png";
+    nomeh1.innerText='pericles';
+    poderP.innerText='fazer chorar';
+    fraquezaP.innerText='Melancolia';
+    Ps.innerText='Ps 245';
+    tipoBt.innerText='não basico';
+})
+Bt4.addEventListener('click',() => {
+    avatar.src = "img/regina case.png";
+    nomeh1.innerText='regina case';
+    poderP.innerText='ixxquenta';
+    fraquezaP.innerText='lugares com arcondicionado ';
+    Ps.innerText='Ps 23';
+    tipoBt.innerText='basico';
+})
+Bt5.addEventListener('click',() => {
+    avatar.src = "img/susana vieria.png";
+    nomeh1.innerText='susana vieria';
+    poderP.innerText='força';
+    fraquezaP.innerText='quebrar tudo';
+    Ps.innerText='Ps 50';
+    tipoBt.innerText='basico';
+})
+
+
+
+
